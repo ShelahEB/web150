7f797c51f5e073346901c571f611ebd05ba4139c
Shelah E Blando <ShelahEB@users.noreply.github.com>
2017-01-05 21:17:30 -0800

    hw1

---
+++ 1_variables_expressions/script.js
@@ -0,0 +1,18 @@
+
+//define variables for:
+
+//firstName,
+    var firstName = "Shelah" ;
+
+//lastName,
+    var lastName = "Blando" ;
+
+//yearsOfStudy, and
+
+    var yearOfStudy = "2 years";
+
+//goal.
+    var goal = "graduate and be employed";
+
+
+//
