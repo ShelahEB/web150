827f648128dc67607f16d70b28b39d18ea01d43a
Shelah Blando <shelah324@gmail.com>
2017-01-10 19:56:31 -0800

    document.write

--- 1_variables_expressions/script.js
+++ 1_variables_expressions/script.js
@@ -12,10 +12,10 @@
     var yearOfStudy = "2 years";
 
 //goal.
-    var goal = "graduate and be employed";
+    var goal = "work as a Web Designer";
 
 
 //message
-var intro = ("My name is " + 'firstName' + 'lastName' + "." + I "I have been at SCC for " + 'yearOfStudy' + "and plan to " + 'goal' + "after completeing my courses.");
-
-document.getElementById('content').innerHTML;
+document.write("My Name is " + firstName + " " + lastName + "<br> <br>");
+document.write("I have been in SCC for " + yearOfStudy + "<br> <br>" );
+document.write("I plan to " + goal + "after completing my courses");
